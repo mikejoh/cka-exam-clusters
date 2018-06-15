e5fc60097e5f0295fed47aef8d8fc614650235c6
Mikael Johansson <mikael.johansson@praqma.com>
2018-06-15 02:09:29 +0200

    Restructure of code

--- control_cluster.sh
+++ control_cluster.sh
@@ -2,16 +2,6 @@
 
 # Start or stop the cluster, to keep your free tier a while! :)
 
-if [[ $1 == "start" ]]
-then
-    start_cluster
-elif [[ $1 == "stop" ]]
-then
-    stop_cluster
-else
-    echo "Huh?"
-fi
-
 start_cluster() {
     echo "Starting instances..."
     gcloud compute instances start --async $(gcloud compute instances list --filter TERMINATED | grep -v "NAME" | awk '{ print $1}')
@@ -21,3 +11,14 @@ stop_cluster() {
     echo "Stopping instances..."
     gcloud compute instances stop --async $(gcloud compute instances list --filter RUNNING | grep -v "NAME" | awk '{ print $1}')
 }
+
+if [[ $1 == "start" ]]
+then
+    start_cluster
+elif [[ $1 == "stop" ]]
+then
+    stop_cluster
+else
+    echo "Not an option..."
+fi
+
